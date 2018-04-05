76ee40351da835975496109fbe0231c02ea24c7a
Jason LaManque <jasonlamanque@gmail.com>
2018-04-05 03:17:28 +0000

    collaborating on lists

---
+++ hello.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+puts "Hello again, changing things"
+
+gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  
+  puts "All I got for valentine's day was #{gift.sample}."
